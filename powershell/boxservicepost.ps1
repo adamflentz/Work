25ce3b89f90e58562e73356c934cd730aa227713
adamflentz <aflentz@email.wm.edu>
2017-07-18 11:44:34 -0400

    added md5 upload to boxservicepost file

--- powershell/boxservicepost.ps1
+++ powershell/boxservicepost.ps1
@@ -10,12 +10,13 @@
 # 3) PATH: Box "path" to place the file
 # 4) ENV: Box environment to place the file (e.g. PRD, PPRD)
 # 5) FILE: path to local file to POST
+# 6) MD5: Option to also upload MD5 File.  'Y' or 'N' required.
 #
 # NOTES: requires Powershell >=3.0
 ######################################################################
 
 #check args length
-if ($args.Length -ne 5)
+if ($args.Length -ne 6)
 {
 	Write-Error "Wrong Command Format: boxservicepost.ps1 <host> <security token> <remote box path> <box environment> <local file path>"
 	Exit 1
@@ -26,7 +27,7 @@ $json_token = $args[1]
 $path = $args[2] 
 $env = $args[3] 
 $file = $args[4] 
-
+$md5 = $args[5]
 
 #read file
 $fileBin = [IO.File]::ReadAllBytes($file)
@@ -62,5 +63,57 @@ try {
 catch [System.Net.WebException] {
     Write-Error( "FAILED to reach '$uri': $_" )
     throw $_
+}
+
+#md5 upload
+if($md5 -e 'Y'){
+	#check powershell version
+	[int]$MajorVersion = $PSVersionTable.PSVersion.Major
+	Write-Host Version $MajorVersion
+
+	#get file path and name from argument
+	$filename = Split-Path $file -leaf
+
+	#converter for powershell 3.0 or less
+	if($MajorVersion -lt 4){
+		Write-Host "Old version.  Files greater than 2.0 GB cannot be converted."
+		$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+		$hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($file)))
+		Write-Host $hash
+	}
+
+	#converter for powershell 4.0 or greater
+	else{
+		Write-Host "Simple algorithm possible"
+		$hash = Get-FileHash $filepath -Algorithm MD5
+		Write-Host $hash.Hash
+		$hash.hash | Out-File ($filename+".md5")
+	}
+
+	#read file
+	$fileBin = [IO.File]::ReadAllBytes($file+".md5")
+	$enc = [System.Text.Encoding]::GetEncoding("iso-8859-1")
+	$fileEnc = $enc.GetString($fileBin)
+
+
+	#create POST body
+	$boundary = [System.Guid]::NewGuid().ToString()    
+	$LF = "`r`n"
+	$bodyLines = (
+		"--$boundary",
+		"Content-Disposition: form-data; name=`"file`"; filename=`"$file`"$LF"+".md5",   
+		$fileEnc,
+		"--$boundary--$LF"
+		) -join $LF
+
+	#invoke the service
+	try {
+		$result = Invoke-WebRequest -UseBasicParsing -Uri $uri -Method POST -Headers $headers -Body $bodyLines -ContentType "multipart/form-data; boundary=`"$boundary`"" 
+		Write-Host $result
+	}
+	catch [System.Net.WebException] {
+		Write-Error( "FAILED to reach '$uri': $_" )
+		throw $_
+	}
 }
 
--- powershell/md5converter.ps1
+++ powershell/md5converter.ps1
@@ -16,10 +16,27 @@ if ($args.Length -ne 1)
 	Exit 1
 }
 
+#check powershell version
+[int]$MajorVersion = $PSVersionTable.PSVersion.Major
+Write-Host Version $MajorVersion
+
+#get file path and name from argument
 $filepath = $args[0]
 $filename = Split-Path $filepath -leaf
-$md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
-$hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($filepath)))
-Write-Host $hash
 
-$hash | Out-File  ($filename+".md5")
+#converter for powershell 3.0 or less
+if($MajorVersion -lt 4){
+    Write-Host "Old version.  Files greater than 2.0 GB cannot be converted."
+    $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+    $hash = [System.BitConverter]::ToString($md5.ComputeHash([System.IO.File]::ReadAllBytes($filepath)))
+    Write-Host $hash
+    $hash | Out-File  ($filename+".md5")
+}
+
+#converter for powershell 4.0 or greater
+else{
+    Write-Host "Simple algorithm possible"
+    $hash = Get-FileHash $filepath -Algorithm MD5
+    Write-Host $hash.Hash
+    $hash.hash | Out-File ($filename+".md5")
+}
