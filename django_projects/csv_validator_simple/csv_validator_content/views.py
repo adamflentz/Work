eade5be14e5db5e5bc3c893bece037c82a30abbc
adamflentz <aflentz@email.wm.edu>
2017-07-27 10:34:00 -0400

    created rendering of webpage with json output in simple csv validator

--- django_projects/csv_validator_simple/csv_validator_content/views.py
+++ django_projects/csv_validator_simple/csv_validator_content/views.py
@@ -2,41 +2,45 @@
 from __future__ import unicode_literals
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+import validate_table, encode_dict
+
+
+
+from wsgiref.util import FileWrapper
+from forms import DocumentForm, JSONDocumentForm
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
-
-from forms import DocumentForm, JSONDocumentForm
 from goodtables import validate
+
+
+import json
+
 
 # Create your views here.
 class HomePageView(TemplateView):
     def post(self, request):
         form = DocumentForm(request.POST, request.FILES)
         jsonform = DocumentForm(request.POST, request.FILES)
-        print(form.is_valid())
         if form.is_valid():
-            self.validate(request)
-            return HttpResponseRedirect(reverse('home'))
+            def validate_table(request):
+                if len(request.FILES) == 1:
+                    jsonfile = None
+                else:
+                    jsonfile = request.FILES['jsonfile']
+                    jsonpath = default_storage.save('tmp/jsonfile.json', ContentFile(jsonfile.read()))
+                csvfile = request.FILES['docfile']
+                path = default_storage.save('tmp/csvfile.csv', ContentFile(csvfile.read()))
+                if jsonfile is None:
+                    validator = validate(path, error_limit=1000000, row_limit=1000000)
+                else:
+                    validator = validate(path, error_limit=1000000, row_limit=1000000, schema=jsonpath)
+                return validator
+            validator_content = validate_table(request)
+            print(validator_content)
+            return render(request, 'home.html', {'validator_content': validator_content})
     def get(self, request):
         form = DocumentForm()  # A empty, unbound form
         jsonform = JSONDocumentForm()
         return render(request, 'home.html', {'form': form, 'jsonform': jsonform})
 
-    def validate(self, request):
-        print("hello ")
-        if len(request.FILES) == 1:
-            jsonfile = None
-        else:
-            jsonfile = request.FILES['jsonfile']
-            jsonpath = default_storage.save('tmp/jsonfile.json', ContentFile(jsonfile.read()))
-        csvfile = request.FILES['docfile']
-        path = default_storage.save('tmp/csvfile.csv', ContentFile(csvfile.read()))
-        if jsonfile is None:
-            validator = validate(path, error_limit=1000000, row_limit=1000000)
-        else:
-            validator = validate(path, error_limit=1000000, row_limit=1000000, schema=jsonpath)
 
-        print(validator)
-        return
